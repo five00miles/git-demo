7fcb5cb051d9fa6ede124c926102bb280f69a9b8
wangjie92 <869719176@qq.com>
2020-12-10 10:05:10 +0800

    1

---
+++ src/components/LoginPopup/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <button @click="handleClick" v-if="show">按钮</button>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      show: false
+    }
+  },
+  methods: {
+    handleClick () {
+      console.log('component $router', this.$router)
+    }
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <button class="home" @click="handleClick">首页</button>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  methods: {
+    handleClick () {
+      this.$loginPopup.show()
+    }
+  }
+}
+</script>
